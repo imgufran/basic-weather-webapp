a2698c1c28aac45ca869966633c83ef659c446eb
Gufran <iamgufran@outlook.com>
2023-05-25 01:05:55 +0530

    update weatherdescription

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (lat, lng, callback) => {
       callback(undefined, "Unable to find location. Try different coordinates.");
     } else {
       // Success
-      callback(undefined, `${body.current.weather_descriptions[0]}! It is currently ${body.current.temperature} celsius out. It feels like ${body.current.feelslike} celsius out`);
+      callback(undefined, `${body.current.weather_descriptions[0]}! It is currently ${body.current.temperature} celsius out. It feels like ${body.current.feelslike} celsius out. The humidity is ${body.current.humidity}%`);
     }
   });
 }
